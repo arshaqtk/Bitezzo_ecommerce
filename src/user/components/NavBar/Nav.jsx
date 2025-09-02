1bb20a1e355a6bf9250e19b6bb3473ec752c4cfe
arshaqtk <150164882+arshaqtk@users.noreply.github.com>
2025-09-02 16:17:57 +0530

    Update Nav.jsx

--- src/user/components/NavBar/Nav.jsx
+++ src/user/components/NavBar/Nav.jsx
@@ -252,7 +252,7 @@ function Nav() {
                         </li>
                         <li
                           className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                          onClick={() => navigate("/orders")}
+                          onClick={() => navigate("/order")}
                         >
                           My Orders
                         </li>
